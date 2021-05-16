8ece25084509f99b793b3ca1f7936fe0782e334f
Tushar Jain <tusharlock10@gmail.com>
2021-05-16 16:40:30 +0530

    fix

--- db/index.js
+++ db/index.js
@@ -4,28 +4,26 @@ const fetch = require("node-fetch");
 const { InMemoryCache } = require("apollo-cache-inmemory");
 
 const link = createHttpLink({
-  uri: process.env.__DEV__
-    ? "http://65.1.155.16:4466"
-    : "http://localhost:4466",
-  fetch: fetch,
+  uri: "http://65.1.155.16:4466",
+  fetch: fetch
 });
 
 const defaultOptions = {
   watchQuery: {
     fetchPolicy: "network-only",
-    errorPolicy: "ignore",
+    errorPolicy: "ignore"
   },
   query: {
     fetchPolicy: "network-only",
-    errorPolicy: "all",
-  },
+    errorPolicy: "all"
+  }
 };
 
 try {
   var client = new ApolloClient({
     link: link,
     cache: new InMemoryCache(),
-    defaultOptions: defaultOptions,
+    defaultOptions: defaultOptions
   });
 
   console.log("Connected to DB");
